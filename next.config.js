675daa356c8e1330fa14c99d30e9efe47c057487
Immanuel Abdi Saputra <immanuel.abdi.saputra@wartek.belajar.id>
2024-04-15 20:33:04 +0700

    add next config

--- next.config.js
+++ next.config.js
@@ -4,5 +4,8 @@ module.exports = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  }
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
